78d4b2af1cfbd5353bc996645992ebd834b47c28
Gustavo Rieper <gustavorieper@gmail.com>
2018-06-21 14:10:34 -0300

    Menu

---
+++ Site_Nesi/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+    </head>
+    <body>
+        <?php include 'menu.php'; ?>
+    </body>
+</html>
---
+++ Site_Nesi/menu.php
@@ -0,0 +1,19 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+</head>
+
+<nav class="menu">
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Sobre</a></li>
+        <li><a href="">Contato</a></li>
+        <li><a href="">Nosso Time</a></li>
+        <li><a href="">Portfólio</a></li>
+        <li><a href="">Blog</a></li>
+        <li style="border-right: solid 1px #FFF;"><a href="">CRM</a></li>    
+        <li style="border-left: none;">
+                <input type="text" name="pesquisa">
+                <input type="button" name="lupa" value="">
+        </li>
+    </ul>
+</nav>
