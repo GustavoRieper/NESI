bd0f4266a431c1d24cd250fa2d9ef5e1a9ecb9c1
Gustavo Rieper <gustavorieper@gmail.com>
2018-06-22 13:57:49 -0300

    Desenvolvimento Tela1
    Desenvolvido alguns elementos da tela 1.

--- Site_Nesi/index.php
+++ Site_Nesi/index.php
@@ -5,5 +5,25 @@
     </head>
     <body>
         <?php include 'menu.php'; ?>
+        <section class="page1">
+            <div class="slogan">
+                <img src="imagens/Logo_Pantone_RGB.png" title="NESI">
+                <h1><span id="row1"></span>Somos a Inteligência<br><span id="row2">por trás da solução.</span></h1>
+            </div>
+            <div class="contact">
+                <span id="ds_contact">Entre em contato conosco:</span><span id="contact">+55 47 3026 8000 | ielusc@nesi.br |
+                <img src="imagens/face.png" title="Página do Facebook"></span>
+            </div>
+            <div class="content">
+                <div class="box1">
+                </div>
+                <div class="box2">
+                </div>
+                <div class="box3">
+                </div>
+                <div class="box4">
+                </div>
+            </div>
+        </section>
     </body>
 </html>
