acde6e847384e44bc8eb5144867c3757f7542e2f
João Luis Medeiros <joaoluismdeiros@gmail.com>
2018-06-26 20:58:47 -0300

    Ajuste Pagina Sobre
    Ajuste no arquivo pagina que foi renomeado para Sobre e ajuste no arquivo CSS para deixar se adequar as seguintes dimensões:
    1920
    1280
    785
    640

--- Site_Nesi/page2.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    </head>
-    <body>
-        <?php include 'menu.php'; ?>
-       
-    </body>
-</html>
---
+++ Site_Nesi/sobre.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="css/sobre.css">
+    </head>
+    <?php include 'menu.php'; ?>
+    <body class="sobreBody">        
+        <div class="sobre">
+            
+            <div class="conteudo">
+            <hr class ="lineTop"/>
+            <!--<div class="texto">-->
+                        <p class="textoP">Lorem ipsum aliquet mattis pellentesque bibendum sem ligula primis, 
+                        leo aliquet amet gravida tristique fusce velit, nisi lorem tortor 
+                        <br>ut velit vulputate nisl. hendrerit mi dui egestas habitasse platea 
+                        per blandit, condimentum pharetra mollis aliquam primis semper sapien ut,
+                         aenean lectus taciti nam nunc dolor. litora velit litora aliquam praesent 
+                        <br> sociosqu hendrerit elit, sodales fusce metus porttitor euismod magna et, senectus 
+                         dictum massa lobortis mattis morbi. eget etiam primis blandit quam ligula 
+                        <br> cubilia pellentesque mauris tristique diam mi, pellentesque velit luctus per 
+                         ultricies magna sodales tempor interdum imperdiet pla-<wbr/>cerat posuere, 
+                         hac ligula vitae lorem tempor fusce cursus fermentum enim taciti
+                         Lorem ipsum aliquet mattis pellentesque bibendum sem ligul-<wbr/>a primis, 
+                        leo aliquet amet gravida tristique fusce velit, nisi lor-<wbr/>em tortor 
+                        ut velit vulputate nisl. hendrerit mi dui egestas habi-<wbr/>tasse platea 
+                        per blandit, condimentum pharetra mollis aliquam pr-<wbr/>mis semper sapien ut,
+                         aenean lectus taciti nam nunc dolor. litora velit lit-<wbr/>ora aliquam praesent 
+                        <br> sociosqu hendrerit elit, sodales fusce metus porttitor euismod -<wbr/>magna et, sen-<wbr/>ectus 
+                         dictum massa lobortis mattis morbi. eget etiam primis blandit-<wbr/> quam ligula 
+                        <br> cubilia pellentesque mauris tristique diam mi, pellentesque velit luctus per 
+                         ultricies magna sodales tempor interdum imperdiet placerat p-<wbr/>osuere, 
+                         hac ligula vitae lorem tempor fusce cursus fermentum enim taciti.     
+                         <br>ut velit vulputate nisl. hendrerit mi dui egestas-<wbr/> ha-<wbr/>bita-<wbr/>sse plat-<wbr/>ea 
+                        tesque velit luctus per 
+                         ultricies magna sodales tempor interdum imperdiet placerat p-<wbr/>osuere, 
+                         hac ligula vitae lorem temp-<wbr/>or fusce cursus fermentum enim ta-<wbr/>citi.
+                         tesque velit luctus per 
+                         ultricies magna sodales tempor interdum imperdiet placerat pos-<wbr/>uere, 
+                         hac ligula vitae lorem tempor fusce cursus fermentum en-<wbr/>im taciti.
+                         tesque velit luctus per 
+                         ultricies magna sodales tempor interdum imperdiet plac-<wbr/>erat po-<wbr/>suere, 
+                         hac ligula vitae lorem tempor fusce cursus fermentum enim taciti.
+                         tesque velit luctus per 
+                         ultricies magna sodales tempor interdum imperdiet placerat posuere, 
+                         hac ligula vitae lorem tempor fusce cursus fermentum <wbr>enim taciti.
+                         <br>tesque velit luctus per 
+                         ultricies magna sodales tempor interdum imperdiet placerat posuere, 
+                         hac ligula vitae lorem tempor fusce cursus fermentum<wbr> enim taciti.                           
+                         hac ligula vitae lorem tempor fusce cursus fermentum enim taciti. 
+                         hac ligula vitae lorem tempor fusce cursus fermentum enim taciti. 
+                         hac ligula vitae lorem tempor fusce cursus fermentum enim taciti.</p>
+            <!--</div>-->
+            <hr class ="lineBottom"/>
+            </div>
+        </div>
+    </body>
+</html>
